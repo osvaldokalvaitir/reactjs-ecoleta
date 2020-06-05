beb1edc87299a6bb231f506b165044a88f83b34a
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2020-06-04 23:37:21 -0300

    feat: concepts react

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
-      </header>
+    <div>
+      <Header title="Hello World" />
+      
+      <h1>Hello World</h1>
     </div>
   );
 }
