ab22675217660346eaaef938cb625e9f55fe506f
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2020-06-06 18:42:20 -0300

    feat: finished frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Routes from './routes';
 
 function App() {
   return (
     <div>
-      <Header title="Hello World" />
-      
-      <h1>Hello World</h1>
+      <Routes />
     </div>
   );
 }
